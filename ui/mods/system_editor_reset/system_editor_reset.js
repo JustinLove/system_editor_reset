66845e3f6919b5a41cdeb83241c3fa3fa96ed40b
Justin Love <git@JustinLove.name>
2014-08-24 02:23:44 -0500

    hijack reload, force biome reload, remember focused planet

---
+++ ui/mods/system_editor_reset/system_editor_reset.js
@@ -0,0 +1,19 @@
+(function() {
+  model.savedSelectedPlanetIndex = ko.observable(-1).extend({session: 'savedSelectedPlanetIndex'})
+  
+  if (model.savedSelectedPlanetIndex() != -1) {
+    setTimeout(function() {
+      model.selectedPlanetIndex(model.savedSelectedPlanetIndex())
+      model.savedSelectedPlanetIndex(-1)
+      model.transitionInToPlanetEditor()
+      model.update_planet()
+    }, 1000)
+  }
+
+  model.api_game_debug_reloadScene = api.game.debug.reloadScene
+  api.game.debug.reloadScene = function() {
+    api.game.setUnitSpecTag(''); // Terrain reloading magic ju-ju
+    model.savedSelectedPlanetIndex(model.selectedPlanetIndex())
+    model.api_game_debug_reloadScene.apply(this, arguments)
+  };
+})()
